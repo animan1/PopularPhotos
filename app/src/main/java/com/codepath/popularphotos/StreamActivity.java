40f3586ca213837b901bb872bb06adf994c8a7de
Daniel Boeve <dboeve@hearsaycorp.com>
2015-05-07 21:02:48 -0700

    Remove menu and default text view

--- app/src/main/java/com/codepath/popularphotos/StreamActivity.java
+++ app/src/main/java/com/codepath/popularphotos/StreamActivity.java
@@ -16,8 +16,6 @@ public class StreamActivity extends ActionBarActivity {
 
   @Override
   public boolean onCreateOptionsMenu(Menu menu) {
-    // Inflate the menu; this adds items to the action bar if it is present.
-    getMenuInflater().inflate(R.menu.menu_stream, menu);
     return true;
   }
 
