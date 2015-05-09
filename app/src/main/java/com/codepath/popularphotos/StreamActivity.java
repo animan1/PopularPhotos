201d38322ff5aeb604ccf60f49f08d83eb7803d7
Daniel Boeve <djboeve@gmail.com>
2015-05-09 14:54:56 -0700

    Adds swipe to refresh

--- app/src/main/java/com/codepath/popularphotos/StreamActivity.java
+++ app/src/main/java/com/codepath/popularphotos/StreamActivity.java
@@ -1,7 +1,9 @@
 package com.codepath.popularphotos;
 
 import android.os.Bundle;
+import android.support.v4.widget.SwipeRefreshLayout;
 import android.support.v7.app.ActionBarActivity;
+import android.view.View;
 import android.widget.ListView;
 
 import java.util.ArrayList;
@@ -13,17 +15,34 @@ public class StreamActivity extends ActionBarActivity {
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_stream);
-    populatePhotos();
+    setupPhotoSwipeContainer();
   }
 
-  private void populatePhotos() {
-    Photo.fetchPopular(new PhotoResponseHandler() {
-      @Override
-      public void onSuccess(ArrayList<Photo> photos) {
-        PhotosAdapter photosAdapter = new PhotosAdapter(StreamActivity.this, photos);
-        ListView photosListView = (ListView)findViewById(R.id.photosListView);
-        photosListView.setAdapter(photosAdapter);
-      }
-    });
+  private void setupPhotoSwipeContainer() {
+    SwipeRefreshLayout photosSwipeContainer = (SwipeRefreshLayout) findViewById(R.id.photosSwipeContainer);
+    PhotoRefreshListener photoRefreshListener = new PhotoRefreshListener(photosSwipeContainer);
+    photosSwipeContainer.setOnRefreshListener(photoRefreshListener);
+    photoRefreshListener.onRefresh();
+  }
+
+  class PhotoRefreshListener implements SwipeRefreshLayout.OnRefreshListener {
+    final SwipeRefreshLayout photosSwipeContainer;
+
+    PhotoRefreshListener(SwipeRefreshLayout photosSwipeContainer) {
+      this.photosSwipeContainer = photosSwipeContainer;
+    }
+
+    @Override
+    public void onRefresh() {
+      Photo.fetchPopular(new PhotoResponseHandler() {
+        @Override
+        public void onSuccess(ArrayList<Photo> photos) {
+          PhotosAdapter photosAdapter = new PhotosAdapter(StreamActivity.this, photos);
+          ListView photosListView = (ListView) findViewById(R.id.photosListView);
+          photosListView.setAdapter(photosAdapter);
+          photosSwipeContainer.setRefreshing(false);
+        }
+      });
+    }
   }
 }
